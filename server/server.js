b7a30ed04a319fa04a36e36511ded10c251d9a59
Deepanshu-Mehta <146458164+Deepanshu-Mehta@users.noreply.github.com>
2024-09-28 22:26:56 +0530

    prodaction test - 1

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const { initializeConnection } = require('./config/database')
 const path = require("path");
+require("bcrypt");
 
 const app = express();
 app.use(express.json());
